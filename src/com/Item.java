693f0f78119d2be3041f3493a78ae646fb622555
Abishek3344 <tathabi8@gmail.com>
2021-03-05 22:56:05 +0530

    commit

--- src/com/Item.java
+++ src/com/Item.java
@@ -56,37 +56,6 @@ public class Item {
 		return output;
 	}
 	
-	public String updateItem(String code, String name, String price, String desc) {
-
-		String output = "";
-		try {
-			Connection con = connect();
-
-			if (con == null) {
-				return "Error while connecting to the database";
-			}
-			// create a prepared statement
-			String query = " UPDATE product SET itemCode=?, itemName=?, itemPrice=?, itemDesc=? WHERE product_id=?";
-					
-			PreparedStatement preparedStmt = con.prepareStatement(query);
-			// binding values
-			preparedStmt.setInt(1, 0);
-			preparedStmt.setString(2, code);
-			preparedStmt.setString(3, name);
-			preparedStmt.setDouble(4, Double.parseDouble(price));
-			preparedStmt.setString(5, desc);
-			// execute the statement
-			preparedStmt.execute();
-			con.close();
-			output = "Inserted successfully";
-		} catch (Exception e) {
-			output = "Error while inserting";
-			System.err.println(e.getMessage());
-		}
-		return output;
-	}
-
-	
 	public String readItems() {
 		String output = "";
 		try {
